2a44bb7c4603fe537266f3b7e8c13e8af896cf25
Rickyxrc <rickyxrc@outlook.com>
2020-12-01 20:45:23 +0800

    luogu-P5019

---
+++ luogu/P5019/20201201204523.c++98.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int last[100005];
+int num=0,n;
+bool tc(int i){
+	bool flag=0;
+//	for(int i=1;i<n;i++){
+		if(last[i]){
+			flag=1;
+			num++;
+			while(last[i]){
+				last[i]--;
+				i++;
+			}
+		}
+//		for(int i=1;i<=n;i++)
+//			cout<<last[i]<<" ";
+//	cout<<endl;
+//	}
+	return flag;
+}
+int main(){
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>last[i];
+	}
+	for(int i=1;i<=n;i++)
+		while(tc(i));
+	cout<<num;
+}
