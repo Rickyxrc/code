b4be93875479b2df76d1c7ffa45c17f62365b1ab
Rickyxrc <rickyxrc@outlook.com>
2020-12-27 13:25:14 +0800

    luogu-P2249

---
+++ luogu/P2249/20201227132514.c++98.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+map<int,int>::iterator it;
+map<int,int>m;
+int main(){
+    ios::sync_with_stdio(false);
+	int n,q,tmp;
+	cin>>n>>q;
+	for(int i=1;i<=n;i++){
+		cin>>tmp;
+		it=m.find(tmp);
+		if(it==m.end())
+			m[tmp]=i;
+	}
+	for(int i=1;i<q;i++){
+		int x;
+		cin>>x;
+		it=m.find(x);
+		if(it!=m.end())
+			cout<<it->second<<" ";
+		else
+			cout<<"-1 ";
+	}
+	int x;
+		cin>>x;
+		it=m.find(x);
+		if(it!=m.end())
+			cout<<it->second;
+		else
+			cout<<"-1";
+	return 0;
+}
