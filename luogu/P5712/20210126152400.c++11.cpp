b52673bb941c90aed7d437b7d2e72ca51fdd20fb
Rickyxrc <rickyxrc@outlook.com>
2021-01-26 15:24:00 +0800

    luogu-P5712

---
+++ luogu/P5712/20210126152400.c++11.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(false);
+	int y;
+    cin>>y;
+    cout<<"Today, I ate "<<y<<" apple";
+    if(y>1)
+        cout<<"s";
+    cout<<".";
+    return 0;
+}
