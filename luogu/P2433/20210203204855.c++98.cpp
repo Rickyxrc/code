eda194f1ccc822e96bce8705ff6493d3903c1484
Rickyxrc <rickyxrc@outlook.com>
2021-02-03 20:48:55 +0800

    luogu-P2433

---
+++ luogu/P2433/20210203204855.c++98.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int T;
+    cin >> T;
+    if (T == 1) {
+        cout << "I love Luogu!";
+    } else if (T == 2) {
+        cout << 2 + 4 << " " << 10 - 2 - 4;
+    } else if (T == 3) {
+    	cout<<3<<endl<<12<<endl<<2;
+    } else if (T == 4) {
+    	cout<<fixed<<setprecision(3)<<500.0/3.0;
+    } else if (T == 5) {
+    	cout<<(260+220)/(12+20);
+    } else if (T == 6) {
+    	cout<<sqrt(6*6+9*9);
+    } else if (T == 7) {
+    	cout<<110<<endl<<90<<endl<<0;
+    } else if (T == 8) {
+    	double pi=3.141593;
+    	cout<<pi*10<<endl<<pi*25<<endl<<(pi*125)*4/3.0;
+    } else if (T == 9) {
+    	cout<<22;
+    } else if (T == 10) {
+    	cout<<9;
+    } else if (T == 11) {
+    	cout<<100.0/3.0;
+    } else if (T == 12) {
+    	cout<<int('M'-'A'+1)<<endl<<char('A'+18-1);
+    } else if (T == 13) {
+    	double pi=3.141593;
+    	cout<<16;
+    } else if (T == 14) {
+    	cout<<50;
+    }
+    return 0;
+}
