056fe1768ae93222ce1c06cefb059344ffde9749
Rickyxrc <rickyxrc@outlook.com>
2021-08-27 09:57:51 +0800

    luogu-P4715

---
+++ luogu/P4715/20210827095751.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int cup[1<<9],val[1<<9],n,cnt=1,num;
+int main(){
+	scanf("%d",&n);
+	n=1<<n;
+	for(int i=1;i<=n;i++)
+		scanf("%d",val+i);
+	num=(n<<1)-1;
+	for(int i=(num>>1)+1;i<=num;i++)
+		cup[i]=cnt++;
+	for(int i=num>>1;i>=1;i--)
+		if(val[cup[(i<<1)]] > val[cup[(i<<1)|1]])
+			cup[i]=cup[(i<<1)];
+		else
+			cup[i]=cup[(i<<1)|1];
+	if(cup[2] == cup[1])
+		printf("%d",cup[3]);
+	else
+		printf("%d",cup[2]);
+	return 0;
+}
