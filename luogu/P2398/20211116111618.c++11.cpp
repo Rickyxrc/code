ad93b1bef91d99d4901868f212dc2c369e415b14
Rickyxrc <rickyxrc@outlook.com>
2021-11-16 11:16:18 +0800

    luogu-P2398

---
+++ luogu/P2398/20211116111618.c++11.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define MAXN 1000001
+using namespace std;
+long long dp[MAXN],ans,n;
+int main(){
+	cin>>n;
+	for(int i=n;i!=0;i--){
+		dp[i]=(n/i)*(n/i);
+		for(int j=i<<1;j<=n;j+=i)dp[i]=dp[i]-dp[j];
+		ans+=dp[i]*i;
+	}
+	cout<<ans;
+	return 0;
+}
