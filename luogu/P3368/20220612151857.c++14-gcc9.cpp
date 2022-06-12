d8b38dc4f7c0dc3081e3bfc5473896326518b80f
Rickyxrc <rickyxrc@outlook.com>
2022-06-12 15:18:57 +0800

    luogu-P3368

---
+++ luogu/P3368/20220612151857.c++14-gcc9.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int dataList[500007],lastTempInput;
+int lowbit(int i){return i&(-i);}
+int getPrefixSum(int index){
+    int returnAns=0;
+    while(index>0){
+        returnAns+=dataList[index];
+        index-=lowbit(index);
+    }
+    return returnAns;
+}
+int tempVar,maxNum,maxCount,tempInput,tempType,tempValue1,tempValue2,tempValue3;
+void updatePrefix(int index,int val){
+    while(index<=maxNum){
+        dataList[index]+=val;
+        index+=lowbit(index);
+    }
+}
+void updatePrefixs(int index,int indexTo,int val){
+    updatePrefix(index,val);
+    updatePrefix(indexTo+1,-val);
+}
+void init(){
+    for(int index=1;index<=maxNum;index++){
+    	scanf("%d",&tempInput);
+        dataList[index]+=tempInput-lastTempInput;
+        lastTempInput=tempInput;
+        tempVar=index+lowbit(index);
+        if(tempVar<=maxNum)dataList[tempVar]+=dataList[index];
+    }
+}
+int main(){
+	scanf("%d%d",&maxNum,&maxCount);
+	init();
+	for(int i=1;i<=maxCount;i++){
+		scanf("%d",&tempType);
+		if(tempType==1){
+			scanf("%d%d%d",&tempValue1,&tempValue2,&tempValue3);
+			updatePrefixs(tempValue1,tempValue2,tempValue3);
+		}
+		else{
+			scanf("%d",&tempValue1);
+			printf("%d\n",getPrefixSum(tempValue1));
+		}
+	}
+	return 0;
+}
