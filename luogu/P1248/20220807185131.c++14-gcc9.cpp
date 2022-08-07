79e0a2c759e8e7cbd9d6f16d0807ce25cd21d194
Rickyxrc <rickyxrc@outlook.com>
2022-08-07 18:51:31 +0800

    luogu-P1248

---
+++ luogu/P1248/20220807185131.c++14-gcc9.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#define Rx2b printf
+#define Rx26 scanf
+#define Rx8d for
+#define Rx81 if
+#define Rx2f auto
+#define Rx3d break
+#define Rxe7 return
+#define Rx47 const
+#define Rxc operator
+#define Rxff +=
+#define Rx90 -=
+#define Rx18 *=
+#define Rx2c /=
+#define Rxb7 >>=
+#define Rx16 <<=
+#define Rx20 >>
+#define Rx1f <<
+#define Rxe ::
+#define Rxd6 ==
+#define Rxa8 ++
+#define Rx39 --
+#define Rx42 int
+#define Rx2e char
+#define Rx6c float
+#define Rxb6 long
+#define Rxea bool
+#define Rxf7 .
+#define Rx4 (
+#define Rx6 )
+#define Rx77 {
+#define Rx46 }
+#define Rx7b [
+#define Rx15 ]
+#define Rx82 <
+#define Rx63 >
+#define Rxfa ;
+#define Rxbd &
+#define Rx37 ,
+#define Rxa6 ?
+#define Rxb2 :
+#define Rx69 +
+#define Rxe3 -
+#define Rxbe *
+#define Rx1 =
+#define Rx92 /
+#define maxNum 10007
+Rx42 minValue Rx4 Rx42 __valueAProps Rx37 
+Rx42 __valueBProps Rx6 Rx77 Rxe7 __valueAProps Rx63 __valueBProps 
+Rxa6 __valueBProps Rxb2 __valueAProps Rxfa Rx46 struct Product 
+Rx77 Rx42 useTimeA Rxfa Rx42 useTimeB Rxfa Rx42 
+productionIdUnique Rxfa Rx42 getMinValue Rx4 Rx6 Rx47 Rx77 
+Rxe7 minValue Rx4 useTimeA Rx37 useTimeB Rx6 Rxfa 
+Rx46 Rxea Rxc Rx82 Rx4 Rx47 Product b 
+Rx6 Rx47 Rx77 Rxe7 getMinValue Rx4 Rx6 Rx82 
+b Rxf7 getMinValue Rx4 Rx6 Rxfa Rx46 Rx46 
+productionsList Rx7b maxNum Rx15 Rxfa std Rxe vector 
+Rx82 Product Rx63 makeProductListFromStart Rx37 makeProductListFromBack Rxfa std 
+Rxe vector Rx82 Product Rx63 makeProductList Rxfa Rx42 
+main Rx4 Rx6 Rx77 Rx42 n Rxfa Rx26 
+Rx4 "%d" Rx37 Rxbd n Rx6 Rxfa Rx8d 
+Rx4 Rx42 __iteratorTempWithIteratorId_In_productionsList Rx1 0 Rxfa __iteratorTempWithIteratorId_In_productionsList Rx82 
+n Rxfa __iteratorTempWithIteratorId_In_productionsList Rxa8 Rx6 Rx26 Rx4 "%d" 
+Rx37 Rxbd productionsList Rx7b __iteratorTempWithIteratorId_In_productionsList Rx15 Rxf7 useTimeA 
+Rx6 Rx37 productionsList Rx7b __iteratorTempWithIteratorId_In_productionsList Rx15 Rxf7 productionIdUnique 
+Rx1 __iteratorTempWithIteratorId_In_productionsList Rx69 1 Rxfa Rx8d Rx4 Rx42 
+__iteratorTempWithIteratorId_In_productionsList Rx1 0 Rxfa __iteratorTempWithIteratorId_In_productionsList Rx82 n Rxfa 
+__iteratorTempWithIteratorId_In_productionsList Rxa8 Rx6 Rx26 Rx4 "%d" Rx37 Rxbd 
+productionsList Rx7b __iteratorTempWithIteratorId_In_productionsList Rx15 Rxf7 useTimeB Rx6 Rxfa 
+std Rxe sort Rx4 productionsList Rx37 productionsList Rx69 
+n Rx6 Rxfa Rx8d Rx4 Rx42 __iteratorTempWithIteratorId_In_productionsList Rx1 
+0 Rxfa __iteratorTempWithIteratorId_In_productionsList Rx82 n Rxfa __iteratorTempWithIteratorId_In_productionsList Rxa8 
+Rx6 Rx81 Rx4 productionsList Rx7b __iteratorTempWithIteratorId_In_productionsList Rx15 Rxf7 
+getMinValue Rx4 Rx6 Rxd6 productionsList Rx7b __iteratorTempWithIteratorId_In_productionsList Rx15 
+Rxf7 useTimeA Rx6 makeProductListFromStart Rxf7 push_back Rx4 productionsList 
+Rx7b __iteratorTempWithIteratorId_In_productionsList Rx15 Rx6 Rxfa else makeProductListFromBack Rxf7 
+insert Rx4 makeProductListFromBack Rxf7 begin Rx4 Rx6 Rx37 
+productionsList Rx7b __iteratorTempWithIteratorId_In_productionsList Rx15 Rx6 Rxfa Rx8d Rx4 
+Rx2f __iteratorTempWithValue_In_makeProductListFromBack Rxb2 makeProductListFromBack Rx6 makeProductListFromStart Rxf7 push_back 
+Rx4 __iteratorTempWithValue_In_makeProductListFromBack Rx6 Rxfa Rx42 needTimeA Rx1 0 
+Rx37 needTimeB Rx1 0 Rxfa Rx8d Rx4 Rx2f 
+__iteratorTempWithValue_In_makeProductListFromStart Rxb2 makeProductListFromStart Rx6 Rx77 needTimeA Rxff __iteratorTempWithValue_In_makeProductListFromStart 
+Rxf7 useTimeA Rxfa Rx81 Rx4 needTimeA Rx63 needTimeB 
+Rx6 needTimeB Rx1 needTimeA Rxfa needTimeB Rxff __iteratorTempWithValue_In_makeProductListFromStart 
+Rxf7 useTimeB Rxfa Rx46 Rx2b Rx4 "%d\n" Rx37 
+needTimeB Rx6 Rxfa Rx8d Rx4 Rx2f __iteratorTempWithValue_In_makeProductListFromStart Rxb2 
+makeProductListFromStart Rx6 Rx2b Rx4 "%d " Rx37 __iteratorTempWithValue_In_makeProductListFromStart 
+Rxf7 productionIdUnique Rx37 __iteratorTempWithValue_In_makeProductListFromStart Rxf7 useTimeA Rx37 __iteratorTempWithValue_In_makeProductListFromStart 
+Rxf7 useTimeB Rx6 Rxfa Rxe7 0 Rxfa Rx46 
+ 
