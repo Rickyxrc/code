ddb39e3a57defa33a970b1f58cd1686a2e6f1156
Rickyxrc <rickyxrc@outlook.com>
2022-08-08 14:12:17 +0800

    luogu-P1000

---
+++ luogu/P1000/20220808141217.c++14-gcc9.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define Rx5AE7 <<
+#define RxBC20 --
+#define Rx6315 .
+#define Rx1011 (
+#define RxEB98 )
+#define RxA1E7 {
+#define RxEBC2 }
+#define RxE722 ;
+#define RxECBF -
+#define Rx58F7 *
+#define RxC7E4 using
+#define RxFD66 namespace
+#define Rx53FD std
+#define RxDD2D int
+#define Rx5F08 main
+#define Rx68CC cout
+#define RxDB4F "                ********"
+#define Rx5658 endl
+#define Rx62DA "               ************"
+#define Rx779C "               ####....#."
+#define Rx54A5 "             #..###.....##...."
+#define Rx0805 "             ###.......######              ###            ###"
+#define RxB5D2 "                ...........               #...#          #...#"
+#define RxE666 "               ##*#######                 #.#.#          #.#.#"
+#define Rx9D6B "            ####*******######             #.#.#          #.#.#"
+#define Rx33A4 "           ...#***.****.*###....          #...#          #...#"
+#define Rx3262 "           ....**********##.....           ###            ###"
+#define Rx265E "           ....****    *****...."
+#define Rx82C3 "             ####        ####"
+#define Rx324E "           ######        ######"
+#define RxE180 "##############################################################"
+#define Rx627B "#...#......#.##...#......#.##...#......#.##------------------#"
+#define RxC42A "###########################################------------------#"
+#define Rx02A5 "#..#....#....##..#....#....##..#....#....#####################"
+#define RxBABC "##########################################    #----------#"
+#define RxE424 "#.....#......##.....#......##.....#......#    #----------#"
+#define RxD341 "#.#..#....#..##.#..#....#..##.#..#....#..#    #----------#"
+#define Rx1E88 "##########################################    ############"
+#define Rx6F86 return
+#define Rx7E66 0
+RxC7E4 RxFD66 Rx53FD RxE722 RxDD2D Rx5F08 Rx1011 RxEB98 
+RxA1E7 Rx68CC Rx5AE7 RxDB4F Rx5AE7 Rx5658 RxE722 Rx68CC 
+Rx5AE7 Rx62DA Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 Rx779C 
+Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 Rx54A5 Rx5AE7 Rx5658 
+RxE722 Rx68CC Rx5AE7 Rx0805 Rx5AE7 Rx5658 RxE722 Rx68CC 
+Rx5AE7 RxB5D2 Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 RxE666 
+Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 Rx9D6B Rx5AE7 Rx5658 
+RxE722 Rx68CC Rx5AE7 Rx33A4 Rx5AE7 Rx5658 RxE722 Rx68CC 
+Rx5AE7 Rx3262 Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 Rx265E 
+Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 Rx82C3 Rx5AE7 Rx5658 
+RxE722 Rx68CC Rx5AE7 Rx324E Rx5AE7 Rx5658 RxE722 Rx68CC 
+Rx5AE7 RxE180 Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 Rx627B 
+Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 RxC42A Rx5AE7 Rx5658 
+RxE722 Rx68CC Rx5AE7 Rx02A5 Rx5AE7 Rx5658 RxE722 Rx68CC 
+Rx5AE7 RxBABC Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 RxE424 
+Rx5AE7 Rx5658 RxE722 Rx68CC Rx5AE7 RxBABC Rx5AE7 Rx5658 
+RxE722 Rx68CC Rx5AE7 RxD341 Rx5AE7 Rx5658 RxE722 Rx68CC 
+Rx5AE7 Rx1E88 RxE722 Rx6F86 Rx7E66 RxE722 RxEBC2 
