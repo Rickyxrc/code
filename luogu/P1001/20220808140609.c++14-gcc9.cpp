695205d2dcb581119d41986c02f444e4ff771f40
Rickyxrc <rickyxrc@outlook.com>
2022-08-08 14:06:09 +0800

    luogu-P1001

---
+++ luogu/P1001/20220808140609.c++14-gcc9.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define RxC6EF (
+#define Rx67B1 )
+#define Rx9033 {
+#define Rx4882 }
+#define RxF918 ;
+#define RxE066 &
+#define RxDBA9 ,
+#define Rx7870 +
+#define RxE782 int
+#define Rx157A main
+#define Rx08EB a
+#define RxAFC8 b
+#define Rx0E77 scanf
+#define RxF35D "%d%d"
+#define Rx61DD printf
+#define Rx5F3D "%d"
+RxE782 Rx157A RxC6EF Rx67B1 Rx9033 RxE782 Rx08EB RxDBA9 
+RxAFC8 RxF918 Rx0E77 RxC6EF RxF35D RxDBA9 RxE066 Rx08EB 
+RxDBA9 RxE066 RxAFC8 Rx67B1 RxF918 Rx61DD RxC6EF Rx5F3D 
+RxDBA9 Rx08EB Rx7870 RxAFC8 Rx67B1 RxF918 Rx4882 
