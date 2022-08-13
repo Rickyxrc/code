2a4f032e0532ff3b11d684fb58ab3b2ea0f9b543
Rickyxrc <rickyxrc@outlook.com>
2022-08-13 16:52:51 +0800

    luogu-easy-B2005

---
+++ luogu-easy/B2005/20220813165251.c++14-gcc9.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    ch = getchar();
+    putchar(' ');
+    putchar(' ');
+    putchar(ch);
+    putchar('\n');
+    putchar(' ');
+    putchar(ch);
+    putchar(ch);
+    putchar(ch);
+    putchar('\n');
+    putchar(ch);
+    putchar(ch);
+    putchar(ch);
+    putchar(ch);
+    putchar(ch);
+    return 0;
+}
