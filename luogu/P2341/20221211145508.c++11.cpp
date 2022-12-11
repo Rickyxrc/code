406721dadabcf27c372a63d6c690c55694de4dfc
Rickyxrc <rickyxrc@outlook.com>
2022-12-11 14:55:08 +0800

    luogu-P2341

---
+++ luogu/P2341/20221211145508.c++11.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <vector>
+#include <stack>
+
+#define maxn 30000
+
+std::vector<int> edge[maxn];
+std::stack<int> st;
+int dfn[maxn], low[maxn], book[maxn], scc[maxn], sizee[maxn], cnt, sccnt;
+
+inline int min(int a, int b) { return (a < b) ? a : b; }
+
+void tarjan(int index)
+{
+  dfn[index] = low[index] = ++cnt;
+  st.push(index);
+  book[index] = 1;
+  for (auto u : edge[index])
+  {
+    if (dfn[u] == 0)
+      tarjan(u),
+          low[index] = min(low[index], low[u]);
+    else if (book[u])
+      low[index] = min(low[index], dfn[u]);
+  }
+  if (dfn[index] == low[index])
+  {
+    sccnt++;
+    while (true)
+    {
+      int u = st.top();
+      st.pop();
+      book[u] = 0;
+      scc[u] = sccnt;
+      sizee[sccnt]++;
+      if (index == u)
+        break;
+    }
+  }
+}
+
+int n, m, u, v, ans, out[maxn], index;
+int main()
+{
+  scanf("%d%d", &n, &m);
+  for (int i = 1; i <= m; i++)
+    scanf("%d%d", &u, &v),
+        edge[u].push_back(v);
+
+  for (int i = 1; i <= n; i++)
+    if (!scc[i])
+      tarjan(i);
+
+  // printf("sccnt = %d\n", sccnt);
+  // for(int i=1;i<=sccnt;i++)
+  //   printf("size[%d] = %d\n",i,sizee[i]);
+
+  for (int i = 1; i <= n; i++)
+    for (auto u : edge[i])
+      if (scc[i] != scc[u])
+      {
+        // printf("%d -> %d\n", scc[i], scc[u]);
+        out[scc[i]]++;
+      }
+  // i,u
+  for (int i = 1; i <= sccnt; i++)
+  {
+    if (out[i] == 0)
+    {
+      if (index)
+        return !puts("0");
+      index = i;
+    }
+  }
+
+  // printf("index = %d\n",index);
+
+  printf("%d", sizee[index]);
+  return 0;
+}
