625024cb3050d8f8e44cccd8b9e07f049bf5dbf8
Rickyxrc <rickyxrc@outlook.com>
2023-02-24 15:18:18 +0800

    luogu-P3048

---
+++ luogu/P3048/20230224151818.c++98.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+#define maxk 13
+
+int st[maxk][1000007];
+
+int LJ(int k,int n){
+	if(k == 1)
+		return 1;
+	
+	if(st[k][n])
+		return st[k][n];
+	
+	int res=1;
+	for(int i=1;i<=n;i++)
+		res += LJ(k-1,i);
+	st[k][n] = res;
+	return res;
+}
+
+int nums[1000007];
+
+void solve(int num,int k){
+	if(k == 1){
+		nums[num-1] = 1;
+		return;
+	}
+	if(num == 1){
+		for(int i=0;i<k;i++)nums[i]=1;
+		return;
+	}
+	int fxid = 1;
+	while(num - LJ(k,fxid) > 1)
+		num -= LJ(k,fxid++);
+	nums[fxid+k-1]=1;
+	solve(num-1,k-1);
+}
+
+int n,k,qd0=1,idx;
+int main(){
+//	freopen("cowids.in" ,"r",stdin );
+//	freopen("cowids.out","w",stdout);
+	scanf("%d%d",&n,&k);
+	solve(n,k);
+	idx=999999;
+	for(int i = 999999;i>=0;i--){
+		if(qd0 && !nums[i])continue;
+		qd0=0;putchar(48|nums[i]);
+	}
+	if(qd0)putchar(48);
+	return 0;
+} 
